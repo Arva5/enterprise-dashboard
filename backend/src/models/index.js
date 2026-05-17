4195fc9f6914a87e75e618baf1f04ff03aec75b3
Atharva M <atharva.mainds83465@gmail.com>
2026-05-18 04:28:25 +0530

    Add backend models index

---
+++ backend/src/models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const LeaveRequest = require('./LeaveRequest');
+const Payroll = require('./Payroll');
+const AuditLog = require('./AuditLog');
+
+module.exports = {
+  User,
+  LeaveRequest,
+  Payroll,
+  AuditLog
+};
